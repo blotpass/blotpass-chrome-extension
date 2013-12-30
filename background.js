83ef3481b33be76f1f39f7a0760e63c5cd47f51e
Stuart P. Bentley <stuart@testtrack4.com>
2013-12-30 02:08:00 -0800

    Correct callback signature

--- background.js
+++ background.js
@@ -23,7 +23,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.url) {
     var hostname = blotpw.getHostnameFromUrl(changeInfo.url);
     if (hostname) {
-      blotpw.getDomainAndInfo(hostname, function(info) {
+      blotpw.getDomainAndInfo(hostname, function(err, info) {
         var email = info.record ? info.record.email
           : info.defaults && info.defaults.email;
         var salt = info.record ? info.record.salt
