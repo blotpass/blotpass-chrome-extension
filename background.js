4d49e630493514cbe0aed67f7ad1d4ce32469cbe
Stuart P. Bentley <stuart@testtrack4.com>
2014-01-03 12:15:22 -0800

    Update all tab icons on install/extension reload

--- background.js
+++ background.js
@@ -3,3 +3,13 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   bloticon.updateTabIcon(tab);
 });
+
+// Update all tabs on startup
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.tabs.query({url: '*://*/*'},
+    function (tabs) {
+      for (var i = 0; i < tabs.length; ++i) {
+        bloticon.updateTabIcon(tabs[i]);
+      }
+    });
+});
